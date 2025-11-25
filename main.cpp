dd6b2494f7fb7f6fdb270c57e376d2d542f84a72
TurtleSL <lincolnhoffman@outlook.com>
2025-11-25 08:20:59 -0600

    first commit

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <SDL3/SDL.h>
+
+int main(int, char**){
+
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window* window = SDL_CreateWindow("blank", 500, 500, 0);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL);
+    
+    SDL_Event e;
+    bool run = true;
+    while(run)
+    {
+        while(SDL_PollEvent(&e))
+        {
+            if(e.type == SDL_EVENT_QUIT)
+            {
+                run = false;
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
